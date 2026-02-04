182eea5c442c562338684c85cca433bd6be5e8bd
jiyuntu <wendy3925@gmail.com>
2026-02-04 15:34:01 +0800

    Fix compilation error in app/page.tsx
    Move style definition to CSS modules
    and use className to reference CSS.

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,14 @@
 import Link from 'next/link';
+import styles from './page.module.css';
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 dark:bg-black">
-      <main style={styles.container}>
-        <Link href="/badminton" style={styles.link}>
+      <main className={styles.container}>
+        <Link href="/badminton" className={styles.link}>
           Badminton
         </Link>
       </main>
     </div>
   );
 }
-
-const styles = {
-  container: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '100vh',
-    fontWeight: 'bold',
-  },
-  link: {
-    fontSize: '2rem',
-    color: 'white',
-    cursor: 'pointer',
-  },
-};
